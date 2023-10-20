016ed7fa4bd0ac654f3582af6ac1a616bb1a2127
Yoel <ygarcialago@danielcastelao.org>
2023-10-20 11:40:30 +0200

    modificao

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Ejercicio de clase
 
 ## modificación de clase
+h
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main{
 	// code here
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello Wrasdtyorld");
     }
 }
